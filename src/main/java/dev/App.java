d79dd7c2092d914eb886c8de36ca0185e08d8d48
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-16 12:03:34 +0200

    Etape 7 : integration d'une librairie tierce (Jfiglet)

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,11 @@
 package dev;
+import com.github.lalyos.jfiglet.FigletFont;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String asciiArt = FigletFont.convertOneLine("App");
+        System.out.println(asciiArt);
     }
 }
