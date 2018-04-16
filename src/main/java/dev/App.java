543c734a06f2b51beb5acbbd1741b0982e617f79
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-16 15:25:22 +0200

    TP 3 sur les profils

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,11 +1,16 @@
 package dev;
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-    	String asciiArt = FigletFont.convertOneLine("App");
-        System.out.println(asciiArt);
+    public static void main(String[] args) {
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+	    String asciiArt = FigletFont.convertOneLine(titre);
+	    System.out.println(asciiArt);
+
+	    // affichage de l'environnement
+	    String environnement = ResourceBundle.getBundle("application").getString("environnement");
+	    System.out.println("Environnement : " + environnement);
     }
 }
